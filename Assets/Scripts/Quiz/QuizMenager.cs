adf7942bfcbe071959b5d75c38295f5f96681d18
Mateusz <rozumpl2@gmail.com>
2026-02-20 23:40:38 +0100

    QuizMenager

--- Assets/Scripts/Quiz/QuizMenager.cs
+++ Assets/Scripts/Quiz/QuizMenager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using IslandQuestions;
 
 public class QuizMenager : MonoBehaviour
 {
@@ -8,5 +9,7 @@ public class QuizMenager : MonoBehaviour
     void QuestionSelector()
     {
         GameObject selectedIsland = islandMenager.spawnedIslands[Random.Range(0, islandMenager.spawnedIslands.Count)];
+        IslandQuestionGenerator islandQuestionGenerator = selectedIsland.GetComponent<IslandQuestionGenerator>();
+        IslandQuestions.Question question = islandQuestionGenerator.GetQuestion();
     }
 }
