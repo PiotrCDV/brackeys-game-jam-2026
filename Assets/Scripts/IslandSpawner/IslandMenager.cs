e7fe3112f5f76ee8b93e27dfe39c1a5cbc8f5d00
Mateusz <rozumpl2@gmail.com>
2026-02-21 18:05:41 +0100

    Rebootcomit

--- Assets/Scripts/IslandSpawner/IslandMenager.cs
+++ Assets/Scripts/IslandSpawner/IslandMenager.cs
@@ -16,8 +16,13 @@ public class IslandMenager : MonoBehaviour
     [SerializeField]
     public List<Transform> spawnPoits;
 
+    public static IslandMenager Instance { get; private set; }
+
     private void Awake()
     {
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
     public void RoundStart()
     {
